adecfbaf4958385869306dc66ffb5b1cd2395779
Alessandra Meddis <45624657+AMeddis@users.noreply.github.com>
2025-01-14 10:40:36 +0100

    Add files via upload

---
+++ Exrecise/Exercise3.R
@@ -0,0 +1,27 @@
+#----------------------------------------------------
+# Exercise 3
+#----------------------------------------------------------------------
+# load the data saved on Wednesday at the end of the day Exercise 2:
+#Example: load("~/Desktop/KVN2021/Course/IntrotoR/data_exercise/db_join.rda")
+# if you cannot find it, use the command: 
+# read.csv("https://raw.githubusercontent.com/AMeddis/IntrotoR-for-Basic-Statistics/refs/heads/main/data_exercise/db_join.csv")
+#-----------------------------------------------------------------------
+
+#------------------------------------------------------------------------
+# We want to plot the diameter growth over time by treatment group
+#-----------------------------------------------------------------------
+
+# 1. Make a boxplot of the diameter change by Day 
+
+
+
+# 2. Calculate the median change by treatment and Day ( you can use the aggregate)
+#     ( store the results in the environment, you would need it for the plot)
+
+
+# 2a. Rename the column "diam.change"  with "median.change" of the data.frame obtained by the aggregate 
+
+
+
+# 2b. Create a plot with the points at the median change from time 0 at varying
+#    of days where the color is defined by the treatment group. Add a legend.
